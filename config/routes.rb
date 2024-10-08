68cd7c7c0ebd31dd18b3d4cd908d532fb5250036
lpwanw <phuongtay2000@gmail.com>
2024-10-08 20:06:31 +0700

    Add Gem GraphQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
